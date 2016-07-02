ba13044ff8dc3c5b64716250052691e15af6ac40
Yury Kotov <bairkan@yandex.ru>
2016-07-02 12:29:57 +0300

    Add some comments

--- lib/scopie_rails/controller.rb
+++ lib/scopie_rails/controller.rb
@@ -10,10 +10,26 @@ module ScopieRails::Controller
     @default_scopie ||= find_scopie_class.new(self)
   end
 
+  # Receives an object where scopes will be applied to.
+  #
+  #   class GraduationsScopie < Scopie::Base
+  #     has_scope :featured, type: :boolean
+  #     has_scope :by_degree, :by_period
+  #   end
+  #
+  #   class GraduationsController < ApplicationController
+  #     include ScopieRails::Controller
+  #
+  #     def index
+  #       @graduations = apply_scopes(Graduation).all
+  #     end
+  #   end
+  #
   def apply_scopes(target, scopie: default_scopie, hash: params)
     Scopie.apply_scopes(target, hash, method: hash[:action], scopie: scopie)
   end
 
+  # Returns the scopes used in this action.
   def current_scopes(scopie: default_scopie, hash: params)
     Scopie.current_scopes(hash, method: hash[:action], scopie: scopie)
   end
